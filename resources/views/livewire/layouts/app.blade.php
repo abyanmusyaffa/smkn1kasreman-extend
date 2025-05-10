e3325ce0e766894c8ed264e56212a7a577d20986
abyanmusyaffa <abyanaufaalifmusyaffa@gmail.com>
2025-05-10 13:40:32 +0700

    add meta tag

--- resources/views/livewire/layouts/app.blade.php
+++ resources/views/livewire/layouts/app.blade.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="utf-8">
  
+        <meta name="description" content="{{ $description }}">
+        <meta name="keywords" content="SMKN 1 Kasreman, Skanka, Skanka Ngawi, SMK Ngawi, Kasreman, SMK Negeri 1 Kasreman, SMK Blue, SMK Biru, TKJ, Akuntansi, Kuliner, Busana, Cangakan, SMK, Sekolah Menengah Kejuruan, Jurusan, SPMB Ngawi">
+        <meta name="author" content="Abyan Aufa Alif Musyaffa">
         <meta name="application-name" content="{{ config('app.name') }}">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
